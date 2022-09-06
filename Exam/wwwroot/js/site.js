a26431e0c2ee44621a2eae2c908c3c501abbf8c6
ermisenes <enes.ermis1@gmail.com>
2022-09-06 21:02:43 +0300

    Add project files.

---
+++ Exam/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿$('#SendBtn').click(function (e) {
+    var articleId = $('#articleId').val();
+   
+    var data = { "id": articleId};
+    $.ajax({
+        url: '/Exam/Check/',
+        type: 'post',
+        data: data,
+       dataType: 'JSON',
+      success: function (dataList) {
+          console.log(dataList.result.length);
+          console.log(dataList.result);
+
+            for (let i = 0; i < dataList.result.length; ++i) {
+                console.log(dataList.result[i]["key"]);
+                var el="#"+dataList.result[i]["key"];
+              $(el).parent().css("background-color", "red");
+            }
+          return;
+        },
+
+    });
+
+
+
+});
